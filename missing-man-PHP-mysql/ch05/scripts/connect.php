5adc7f2a5bb3511fe35f412d6b5b709df6dbb377
Artem Kovalyk <artre8@gmail.com>
2015-10-28 16:53:12 -0400

    connecting to a database

--- missing-man-PHP-mysql/ch05/scripts/connect.php
+++ missing-man-PHP-mysql/ch05/scripts/connect.php
@@ -1,17 +1,27 @@
 <?php
-	mysql_connect("localhost", "root", "root")
+	require '../../scripts/app_config.php';
+	
+	mysql_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD)
 		or die("<p>Error connecting to database: " . mysql_error() . "</p>");
 	
 	echo "<p>Connected to MySQL!";
 	
-	mysql_select_db("missing_manual_php_msql")
-		or die("<p>Error selecting the database missing_manual_php_msql" . mysql_error() . "</p>");
+	mysql_select_db(DATABASE_NAME)
+		or die("<p>Error selecting the database " . DATABASE_NAME . mysql_error() . "</p>");
 		
-	echo "<p>Connected to MySQL, using database missing_manual_php_msql.</p>";	
+	echo "<p>Connected to MySQL, using database " . DATABASE_NAME . "</p>";	
 	
 	$result = mysql_query("SHOW TABLES;");
 	
 	if (!$result) {
 		die("<p>Error in listing tables: " . mysql_error() . "</p>");
 	}
+	echo "<p>Tables in database:</p>";
+	echo "<ul>";
+	while ($row = mysql_fetch_row($result)) {
+		echo "<li>Table: " . $row[0] . "</li>";
+	}
+	echo "</ul>";
+	
+	
 ?>
