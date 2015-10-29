2648920f41feb23991f92d757d3126c66557cf9b
Artem Kovalyk <artre8@gmail.com>
2015-10-29 12:29:57 -0400

    query Runner, and regular expressions

---
+++ missing-man-PHP-mysql/scripts/database_connection.php
@@ -0,0 +1,14 @@
+<?php
+	require 'app_config.php';
+	
+	mysql_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD)
+		or die("<p>Error connecting to database: " . mysql_error() . "</p>");
+		
+	echo "<p>Connected to MySQL</p>";
+	
+	mysql_select_db(DATABASE_NAME)
+		or die("<p>Error selecting a database " . DATABASE_NAME . mysql_error() .  "</p>");
+		
+	echo "Connected to MySQL, using database: " . DATABASE_NAME . "</p>";
+	
+?>
